d5ecd71481d87e3f26896b50362138c57e1bda31
ayush <ayushpandey667@gmail.com>
2024-12-16 20:54:29 +0530

    backend chnges done API changes update adn delete

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,10 +4,13 @@ const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
+var fetchuser = require('../middleware/fetchuser');
 
 const JWT_SECRET = "ayushisagoodb$oy";
-// Create a user using POST "/api/auth", doesn't require authentication
-router.post('/', [
+
+
+// ROUTE 1:Create a user using POST "/api/auth/createuser", doesn't require authentication
+router.post('/createuser', [
   body('name', 'Enter a valid name').isLength({ min: 3 }),
   body('email', "Enter a valid Email").isEmail(),
   body('password', 'Password must have a minimum of 5 characters').isLength({ min: 5 }),
@@ -40,5 +43,59 @@ router.post('/', [
     res.status(500).json({ error: 'Server error' });
   }
 });
+
+
+
+
+// ROUTE 2: Authenticate a user using POST "/api/auth/login", doesn't require authentication
+router.post('/login', [
+  body('email', "Enter a valid Email").isEmail(),
+  body('password', "Password cannont be blank").exists(),
+] , async (req, res) => {
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  } 
+
+  const {email,password} = req.body
+try {
+   let user = await  User.findOne({email})
+   if(!user){
+    return res.status(400).json({error:"PLease login with correct credentials"})
+   }
+   let passCompare = await  bcrypt.compare(password,user.password)
+   if(!passCompare){
+    return res.status(400).json({error:"PLease login with correct credentials"})
+   }
+
+   const data = {
+    user:{
+      id:user.id
+    }
+  }
+  const authToken = jwt.sign(data,JWT_SECRET)
+  res.json({authToken});
+} catch (error) {
+  res.status(500).send({ error: 'Internal Server Error' });
+}
+})
+
+
+
+
+
+// ROUTE 3: Get user details  using POST "/api/auth/getuser", Log in Required
+
+router.post('/getuser', fetchuser , async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const user = await User.findById(userId).select("-password");
+    res.send(user);
+  }  catch (error) {
+    res.status(500).send({ error: 'Internal Server Error' });
+  }
+})
+
 
 module.exports = router;
