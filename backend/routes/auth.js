dacfb09878816bc1f75bf887cb2edb40c87587bc
ayush <ayushpandey667@gmail.com>
2025-01-11 14:00:51 +0530

    feat: enhance signup functionality with user validation and improved alert handling

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -15,14 +15,19 @@ router.post('/createuser', [
   body('email', "Enter a valid Email").isEmail(),
   body('password', 'Password must have a minimum of 5 characters').isLength({ min: 5 }),
 ], async (req, res) => {
+  let success = false;
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+    return res.status(400).json({ success, errors: errors.array() });
   } 
   const salt = await bcrypt.genSalt(10);
   const secPass =  await bcrypt.hash(req.body.password, salt);
   try {
-    const user = await User.create({
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res.status(400).json({ success, error: "Sorry a user with this email already exists" });
+    }
+     user = await User.create({
       name: req.body.name,
       email: req.body.email,
       password: secPass
@@ -33,7 +38,8 @@ router.post('/createuser', [
       }
     }
     const authToken = jwt.sign(data,JWT_SECRET)
-    res.json({authToken});
+    susccess = true;
+    res.json({success,authToken});
   } catch (error) {
     if (error.code === 11000) {
       // Duplicate key error
--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,24 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React from 'react'
 
 function Alert(props) {
-    const [alert, setAlert] = useState(props.message);
-
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setAlert("");
-        }, 2000);
-        return () => clearTimeout(timer);
-    }, []);
+    const capitalize = (word)=>{
+        const lower = word.toLowerCase();
+        return lower.charAt(0).toUpperCase() + lower.slice(1);
+    }
     return (
-        <>
-            <div
-                className={`alert alert-primary ${alert ? "" : "d-none"}`}
-                role="alert"
-            >
-                {alert}
-            </div>
-        </>
-    );
+        <div style={{height: '50px'}}>
+        {props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+           <strong>{capitalize(props.alert.type)}</strong>: {props.alert.msg} 
+        </div>}
+        </div>
+    )
 }
 
-export default Alert;
+export default Alert
